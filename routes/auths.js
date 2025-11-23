1755df776d134b5b0403f785556dfa77477bc20b
Samuel Popoola <samuelpopoola323@gmail.com>
2025-11-23 21:50:22 +0100

    Update auths.js

--- routes/auths.js
+++ routes/auths.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const User = require("../models/User");
+const User = require("../model/user");
 
 // -----------------------------
 // 1. Demo login (anonymous user)
